236da076db9113838c44ea3a48a1b011f7bcdbd9
parse_shyam <parse.s@simformsolutions.com>
2020-07-26 01:38:27 +0530

    added socket to main server.

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,10 @@ let server = spdy.createServer({
     cert: fs.readFileSync('./config/server.crt'),
 }, app);
 
+export const io = IO(server); // set up socket.io and bind it to our http server.
+
 server.listen(PORT, () => {
     console.log(`SERVER IS UP ON PORT ${PORT}`)
 });
 
-export const io = IO(server); // set up socket.io and bind it to our http server.
+require('./services/socket.service').socketInit(io);
