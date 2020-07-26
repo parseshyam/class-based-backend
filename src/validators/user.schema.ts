b7452e20412e2ee31f7203659a2b9e79ee43e50e
parse_shyam <parse.s@simformsolutions.com>
2020-07-26 06:34:37 +0530

    added validator comment

--- src/validators/user.schema.ts
+++ src/validators/user.schema.ts
@@ -1,15 +1,20 @@
-import { body } from 'express-validator';
+import { body, param } from 'express-validator';
 
+// alawys sanitizatize the input use trim and escape every for each input 
 const loginSchema = [
     body('email')
-        .exists().withMessage('user name is required!')
-        .bail()
+        .trim()
+        .escape()
         .isEmail()
         .normalizeEmail(),
     body('text')
-        .exists().withMessage('text is required!')
         .trim()
         .escape(),
+    param('hello')
+        .trim()
+        .escape()
+        // .isNumeric()
+
 ]
 
 export default {
