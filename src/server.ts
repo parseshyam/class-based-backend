d2ae99526b890ef7b30ca5a5a2699fbc3fba951d
parse_shyam <parse.s@simformsolutions.com>
2020-07-26 06:22:05 +0530

    changed folder name

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,8 @@ config();
 let PORT = process.env.PORT || 7000;
 
 let server = spdy.createServer({
-    key: fs.readFileSync('./config/key.pem'),
-    cert: fs.readFileSync('./config/server.crt'),
+    key: fs.readFileSync('./keys/key.pem'),
+    cert: fs.readFileSync('./keys/server.crt'),
 }, app);
 
 export const io = IO(server); // set up socket.io and bind it to our http server.
