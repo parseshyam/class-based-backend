8c3c5f32f6c4c77b746a176ccf3f64f8e0681e66
parse_shyam <parse.s@simformsolutions.com>
2020-07-27 16:42:07 +0530

    code refactoring and moved back to http/1.1

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,9 @@ import { sequelize } from './configs/db.config'
 import logger from './services/logger';
 class App {
     public app: express.Application;
-    // all the routes goes here.
+    // all the routes goes here
     public user: UserRoutes = new UserRoutes();
+    // initilize express app with prerequsite
     constructor() {
         this.app = express.default();
         this.initializeDB();
@@ -14,30 +15,32 @@ class App {
         this.user.routes(this.app);
         this.errorHandler();
     }
-
+    // initilize all the middlewares.
     private initilizeMiddleware = () => {
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: false }));
         this.app.use(cors())
     }
-
+    // initilize the db instance
     private initializeDB = async () => {
         import('./models')
             .then(_ => sequelize.authenticate())
             .then(_ => logger.info('DB Connected successfully.'))
             .catch(error => logger.error(error.message))
     }
-
+    // initilize centeralized error handler for db and express errors.
     private errorHandler = () => {
+        // error handler for undefined routes
         this.app.use((req, res, next) => {
-            const error: any = new Error('URL NOT FOUND');
+            const error: any = new Error(`${req.originalUrl} not found.`);
             error.statusCode = 404;
             error.data = {}
             next(error);
         });
+        // error handler to catch all db and validations errors
         this.app.use((error: any, req: any, res: any, next: any) => {
             if (error?.original) {
-                error.message = error.original?.detail || error.original?.routine || "db error." ;
+                error.message = error.original?.detail || error.original?.routine || "db error.";
                 error.data = error?.errors?.length > 0 ? error.errors.filter((e: any) => delete e.instance) : []
             }
             return res.status(error.statusCode || 500).json({
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,11 @@
 import app from './app'
-// import spdy from 'spdy';
-import { createServer } from 'http';
-import fs from 'fs';
 import IO from 'socket.io';
 import logger from './services/logger';
 import { config } from 'dotenv';
+import { socketInit } from './services/socket.service';
 config();
 
 let PORT = process.env.PORT || 7000;
-
-// let server = spdy.createServer({
-//     key: fs.readFileSync('./keys/key.pem'),
-//     cert: fs.readFileSync('./keys/server.crt'),
-// }, app);
-let server = createServer(app);
-
-export const io = IO(server); // set up socket.io and bind it to our http server.
-
-server.listen(PORT, () => {
-    logger.info(`Http server is up and running on port -> ${PORT}`);
-});
-
-require('./services/socket.service').socketInit(io, PORT);
+let server = app.listen(PORT, () => logger.info(`Http server is up and running on port -> ${PORT}`));
+export const io = IO(server);
+socketInit(io, PORT);
